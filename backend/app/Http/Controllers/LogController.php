69d975a6bc967d71cf7a2cf0070a75998c135c78
ext.scova <ext.scova@aper.com>
2025-09-30 12:10:28 -0300

    Update api controller

--- backend/app/Http/Controllers/LogController.php
+++ backend/app/Http/Controllers/LogController.php
@@ -4,17 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Log;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class LogController extends Controller
 {
-    public function index(): JsonResponse
-    {
-        $logs = Log::all();
+    private $ttl = 900; // 15 minutes in seconds    
 
-        foreach ($logs as $log) {
-            $log->logLevel;
-        }
+    public function apiResponse($code, $message, $data = []){
+        return response()->json([
+            'code' => $code,
+            'message' => $message,            
+            'data' => $data
+        ], $code);
+    }
 
-        return response()->json($logs);
+    public function healthcheck(){
+        return $this->apiResponse(200, 'API funcionando correctamente');
+    }
+
+    public function index(Request $request): JsonResponse
+    {           
+        $query = $request->input('query');
+        $level = $request->input('level');
+        $paginate = $request->input('paginate');
+                        
+        $logs = Log::whereHas('logLevel',function ($q) use ($level) {
+                $q->when($level, function ($q) use ($level) {
+                    $q->whereLike('name', '%'.$level.'%');
+                });
+            })
+            ->when($query, function ($q) use ($query) {
+                $q->whereLike('message', '%'.$query.'%');
+            })            
+            ->with('logLevel')
+            ->paginate($paginate);
+        
+        return $this->apiResponse(200, 'success', $logs);
     }
 }
