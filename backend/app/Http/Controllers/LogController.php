69c7610c067d6e51c0d4ad335d5c1c42afa8223e
ext.scova <ext.scova@aper.com>
2025-09-30 12:13:13 -0300

    delete facade cache

--- backend/app/Http/Controllers/LogController.php
+++ backend/app/Http/Controllers/LogController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Log;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class LogController extends Controller
 {    
